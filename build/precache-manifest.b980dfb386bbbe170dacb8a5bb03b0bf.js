75e50bec8c916d0c1debb1b421a465c8af83e7a9
fedorovbb <Boris.FEDOROV@raiffeisen.ru>
2020-02-12 17:27:05 +0300

    Added a Procfile.

---
+++ build/precache-manifest.b980dfb386bbbe170dacb8a5bb03b0bf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3b0661b5c763e975c22e6c1db6653ca",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cc8fd87a5082be798aa8",
+    "url": "/static/css/main.e8a27cb2.chunk.css"
+  },
+  {
+    "revision": "ff1f0db9bc3e67211c7f",
+    "url": "/static/js/2.5b792e95.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.5b792e95.chunk.js.LICENSE"
+  },
+  {
+    "revision": "cc8fd87a5082be798aa8",
+    "url": "/static/js/main.56d359a9.chunk.js"
+  },
+  {
+    "revision": "d83964782b9d575e2b1f",
+    "url": "/static/js/runtime-main.290269e8.js"
+  },
+  {
+    "revision": "2ef1f2b76403ae0674bc16ba655b9a45",
+    "url": "/static/media/spinner.2ef1f2b7.svg"
+  }
+]);
